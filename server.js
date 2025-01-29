fe677e10aa8e8908dce823b69881ae8d5fe801e8
Oluwatise <141239844+Oluwatise-Ajayi@users.noreply.github.com>
2025-01-29 23:26:11 +0100

    edit

--- server.js
+++ server.js
@@ -5,14 +5,15 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/api/data", (req, res) => {
   const response = {
     email: "hannahjesuloluwa@gmail.com",
     current_datetime: new Date().toISOString(),
     github_url: "https://github.com/Oluwatise-Ajayi/HNG_Stage_Projects",
   };
-  res.json(response);
+  res.status(200).json(response);
 });
 
 app.listen(PORT, () => {
